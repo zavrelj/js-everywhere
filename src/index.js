fbb1ae2f7ba9d92f151354171cc08d7195cf51c4
Jan Zavrel <jan@zavrel.net>
2020-03-10 12:27:21 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// index.js
+// This is the main entry point of our application
+
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => res.send('Hello Web Server!!!'));
+
+app.listen(port, () =>
+  console.log('Server running at http://localhost:${port}')
+);
